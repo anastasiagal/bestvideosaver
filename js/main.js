2d0b8db103c85b4ab72d0e86e0c843efdb5bd7f2
unknown <anastasya.galyamova@gmail.com>
2022-05-13 23:03:36 +0700

    medea, blog3, blog4

---
+++ js/main.js
@@ -0,0 +1,4 @@
+function changelang(a) {
+    a.classList.toggle("menu__list-link--rotate");
+    document.getElementsByClassName('select-lang__list')[0].classList.toggle("select-lang__list--hidden"); 
+}
