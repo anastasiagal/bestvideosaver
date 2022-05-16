dc111d821cc32db0fb0259c4d14739f6a5329935
unknown <anastasya.galyamova@gmail.com>
2022-05-16 17:48:10 +0700

    added preloader

--- js/main.js
+++ js/main.js
@@ -2,3 +2,13 @@ function changelang(a) {
     a.classList.toggle("menu__list-link--rotate");
     document.getElementsByClassName('select-lang__list')[0].classList.toggle("select-lang__list--hidden"); 
 }
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  async function preloader() {
+      document.getElementById('preloader').classList.add("loading-block--active");
+      await sleep(3000);
+      document.getElementById('preloader').classList.remove("loading-block--active");
+  }
