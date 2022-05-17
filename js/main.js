486a5c0da7cc7a452ab005d59867d6debad42326
unknown <anastasya.galyamova@gmail.com>
2022-05-17 14:09:09 +0700

    fix dropdown menu

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ function changelang(a) {
     document.getElementsByClassName('select-lang__list')[0].classList.toggle("select-lang__list--hidden"); 
 }
 
+function myFunction(x) {
+    x.classList.toggle("change");
+   document.getElementsByClassName('menu-drop__list')[0].classList.toggle("menu-drop__list--active");
+  };
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
